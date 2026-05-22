e1996f146a358d608a5d5c07dfb5cc098123d6d1
apekhnik <iam3ji0@gmail.com>
2026-05-22 21:04:55 +0300

    feat: redirect root to /home for all visitors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 
-export default async function RootPage() {
-  const { userId } = await auth()
-  if (userId) redirect('/home')
-  else redirect('/sign-in')
+export default function RootPage() {
+  redirect('/home')
 }
