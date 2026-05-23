eac6f64176620c9e98bd3b47dfef0d380592d6c1
apekhnik <iam3ji0@gmail.com>
2026-05-23 13:08:31 +0300

    feat: show public recipes to guests on /home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,12 @@
 import { auth } from '@clerk/nextjs/server'
-import { getRecipesWithRelations } from '@/actions/recipes'
-import { DEMO_RECIPES } from '@/lib/demo-data'
+import { getRecipesWithRelations, getPublicRecipes } from '@/actions/recipes'
 import AppShell from '@/components/AppShell'
 import RecipeFeed from '@/components/RecipeFeed'
 import DesktopHome from '@/components/desktop/DesktopHome'
 
 export default async function HomePage() {
   const { userId } = await auth()
-  const recipes = userId ? await getRecipesWithRelations() : DEMO_RECIPES
+  const recipes = userId ? await getRecipesWithRelations() : await getPublicRecipes()
 
   return (
     <AppShell>
