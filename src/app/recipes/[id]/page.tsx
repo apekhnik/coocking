13fb31dace5320ee5e3ae03a96ea491a3e23d694
apekhnik <iam3ji0@gmail.com>
2026-05-23 13:16:47 +0300

    feat: allow guests to view public recipes in read-only mode

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -1,21 +1,33 @@
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
-import { getRecipe } from '@/actions/recipes'
+import { getRecipe, getPublicRecipe } from '@/actions/recipes'
 import AppShell from '@/components/AppShell'
 import RecipeDetailView from '@/components/RecipeDetailView'
 
 export default async function RecipeDetailPage({ params }: { params: Promise<{ id: string }> }) {
   const { userId } = await auth()
-  if (!userId) redirect('/sign-in')
-
   const { id } = await params
-  const recipe = await getRecipe(id)
-  if (!recipe) redirect('/home')
+
+  if (userId) {
+    const recipe = await getRecipe(id)
+    if (!recipe) redirect('/home')
+    return (
+      <AppShell>
+        <div className="flex-1 overflow-hidden">
+          <RecipeDetailView recipe={recipe} />
+        </div>
+      </AppShell>
+    )
+  }
+
+  // Guest: only public recipes are accessible
+  const recipe = await getPublicRecipe(id)
+  if (!recipe) redirect('/sign-in')
 
   return (
     <AppShell>
       <div className="flex-1 overflow-hidden">
-        <RecipeDetailView recipe={recipe} />
+        <RecipeDetailView recipe={recipe} readOnly showSignupBanner />
       </div>
     </AppShell>
   )
