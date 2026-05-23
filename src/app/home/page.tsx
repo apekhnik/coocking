043e71fb048f7ed54ec36604afb168fa15ffe895
apekhnik <iam3ji0@gmail.com>
2026-05-23 16:08:15 +0300

    feat: home page is now a pure community feed

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,20 +1,18 @@
-import { auth } from '@clerk/nextjs/server'
-import { getRecipesWithRelations, getPublicRecipes } from '@/actions/recipes'
+import { getPublicRecipes } from '@/actions/recipes'
 import AppShell from '@/components/AppShell'
 import RecipeFeed from '@/components/RecipeFeed'
 import DesktopHome from '@/components/desktop/DesktopHome'
 
 export default async function HomePage() {
-  const { userId } = await auth()
-  const recipes = userId ? await getRecipesWithRelations() : await getPublicRecipes()
+  const recipes = await getPublicRecipes()
 
   return (
     <AppShell>
       <div className="flex-1 lg:hidden overflow-hidden">
-        <RecipeFeed recipes={recipes} />
+        <RecipeFeed recipes={recipes} disableActions />
       </div>
       <div className="hidden lg:flex flex-1 overflow-hidden">
-        <DesktopHome recipes={recipes} />
+        <DesktopHome recipes={recipes} disableActions />
       </div>
     </AppShell>
   )
